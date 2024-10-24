f613e39957ecfb79940afbc406f62bea05a7c5b6
Ishaan's Macbook Air <admin@scizers.com>
2024-10-24 15:31:01 +0400

    /map/event_hyderabad

--- src/middleware.js
+++ src/middleware.js
@@ -7,21 +7,9 @@ export const config = {
 export function middleware(req) {
   const { device } = userAgent(req);
   const { pathname } = req.nextUrl;
-
   const viewport = device.type === "mobile" ? "mobile" : "desktop";
+  req.nextUrl.pathname = `/${viewport}${pathname}`;
+  return NextResponse.rewrite(req.nextUrl);
 
-  const hasSeenPopup = req.cookies.get("hasSeenPopup") === "true";
-
-  if (!hasSeenPopup) {
-    if (viewport === "mobile") {
-      // return NextResponse.redirect(new URL("/mobile-popup", req.url));
-      return NextResponse.redirect(new URL("/mobile", req.url));
-    } else {
-      // return NextResponse.redirect(new URL("/desktop-popup", req.url));
-      return NextResponse.redirect(new URL("/desktop", req.url));
-    }
-  } else {
-    req.nextUrl.pathname = `/${viewport}${pathname}`;
-    return NextResponse.rewrite(req.nextUrl);
-  }
 }
+
